74ccdd687dfd04212531365d805cfebd2259aa4c
Douglas Harvey <46091998+douglas-m-harvey@users.noreply.github.com>
2023-01-05 00:30:59 +0000

    Updated noise/perlin.py, it's much faster now!

--- noise/perlin.py
+++ noise/perlin.py
@@ -8,33 +8,11 @@ import matplotlib.pyplot as plt
 
 
 
-def angle(p0, p1):
-    return np.arctan2(p1[0] - p0[0], p1[1] - p0[1])
-
-
 def unit_vector(angle):
-    return (np.sin(angle), np.cos(angle))
-
-
-def vector(p0, p1):
-    return (p1[0] - p0[0], p1[1] - p0[1])
-
-
-def corners(point):
-    return np.array([[np.floor(point[0]), np.floor(point[1])],
-                     [np.floor(point[0]), np.ceil(point[1])],
-                     [np.ceil(point[0]), np.floor(point[1])],
-                     [np.ceil(point[0]), np.ceil(point[1])]]).astype(int)
-
+    return np.array([np.sin(angle), np.cos(angle)])
 
 def smoothstep(x):
     return 6*x**5 - 15*x**4 + 10*x**3
-
-
-def value(point, point_dots):
-    interp_x_t = np.interp(smoothstep(point[1]%1), [0, 1], point_dots[:2])
-    interp_x_b = np.interp(smoothstep(point[1]%1), [0, 1], point_dots[2:])
-    return np.interp(smoothstep(point[0]%1), [0, 1], [interp_x_t, interp_x_b])
 
 
 
@@ -56,26 +34,60 @@ shape_grid = np.array([params["grid_y"], params["grid_x"]])
 shape_image = np.array([params["image_y"], params["image_x"]])
 
 
-grid_vectors = np.zeros((2, shape_grid[0] + 1, shape_grid[1] + 1))
-for index in np.ndindex(grid_vectors.shape[1:]):
-    grid_vectors[:, *index] = unit_vector(rn.uniform(0, 2*np.pi))
-if params["tile_y"]: grid_vectors[:, -1, :] = grid_vectors[:, 0, :]
-if params["tile_x"]: grid_vectors[:, :, -1] = grid_vectors[:, :, 0]
+# Define a grid of random unit vectors, with equal edge vectors so image can be tiled.
+vectors = unit_vector(rn.uniform(0, 2*np.pi, shape_grid + 1))
+vectors[:, -1, :] = vectors[:, 0, :]
+vectors[:, :, -1] = vectors[:, :, 0]
 
-points_y = np.linspace(0, shape_grid[0], shape_image[0])
-points_x = np.linspace(0, shape_grid[1], shape_image[1])
-points = np.array(np.meshgrid(points_x, points_y))
-points[0, ...] %= shape_grid[1]
-points[1, ...] %= shape_grid[0]
+# Define a grid of sample points.
+points = np.array(np.meshgrid(np.linspace(0, shape_grid[1], shape_image[1]),
+                              np.linspace(0, shape_grid[0], shape_image[0])))
 
-image = np.zeros(shape_image)
-for index in np.ndindex(*shape_image):
-    point = points[:, *index][::-1]
-    point_dots = [np.dot(grid_vectors[:, *corner], vector(corner, point))
-                  for corner in corners(point)]
-    image[index] = value(point, point_dots)
-    progress = (index[0]*shape_image[1] + index[1])/shape_image.prod()
-    print("Progress: {:2.1%}".format(progress), end = "\r")
+# For each sample point, find the top, bottom, left and right edges of its cell.
+# Format: [shape_image]
+t = np.floor(points[0]).astype(int)
+b = np.ceil(points[0]).astype(int)
+l = np.floor(points[1]).astype(int)
+r = np.ceil(points[1]).astype(int)
+
+# For each sample point, find the gradient vectors of its cell.
+# Format: [2, *shape_image]
+gv_tl = np.array([vectors[0][l, t], vectors[1][l, t]])
+gv_tr = np.array([vectors[0][r, t], vectors[1][r, t]])
+gv_bl = np.array([vectors[0][l, b], vectors[1][l, b]])
+gv_br = np.array([vectors[0][r, b], vectors[1][r, b]])
+
+# Define an array containing gradient vectors for every point.
+# Format: [4, 2, *shape_image]
+gv = np.array([gv_tl, gv_tr, gv_bl, gv_br])
+
+# For each sample point, calculate the distances to the corners of its cell.
+# Format: [shape_image]
+d_t = t - points[0]
+d_b = b - points[0]
+d_l = l - points[1]
+d_r = r - points[1]
+
+# Define the distance vectors of each point.
+# Format: [2, *shape_image]
+dv_tl = np.array([d_t, d_l])
+dv_tr = np.array([d_t, d_r])
+dv_bl = np.array([d_b, d_l])
+dv_br = np.array([d_b, d_r])
+
+# Define an array containing distance vectors for every point.
+# Format: [4, 2, *shape_image]
+dv = np.array([dv_tl, dv_tr, dv_bl, dv_br])
+
+# Calculate the dot product between each point's distance vectors and its cell's gradient vectors.
+# Format: [4, *shape_image]
+dots = dv[:, 0, :, :]*gv[:, 0, :, :] + dv[:, 1, :, :]*gv[:, 1, :, :]
+
+# Interpolate between each point's dot products to define the image.
+# Format[shape_image]
+interp_x_t = dots[0, ...] + smoothstep(points[1, ...]%1)*np.subtract(dots[1, ...], dots[0, ...])
+interp_x_b = dots[2, ...] + smoothstep(points[1, ...]%1)*np.subtract(dots[3, ...], dots[2, ...])
+image = interp_x_t + smoothstep(points[0, ...][::-1]%1)*np.subtract(interp_x_b, interp_x_t)
 
 
 
